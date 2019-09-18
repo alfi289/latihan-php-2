f673d892775002efc3f0f12d4de1a4671d9ce411
Alfi Yudin <alfiyudin46@gmail.com>
2019-09-18 20:16:10 +0700

    latihan

---
+++ tugas-1.php
@@ -0,0 +1,13 @@
+<?php
+$a = 15000;
+$b = 6;
+$c = 5;
+$d = 2000;
+$e = ($b*$c)*$a;
+$f = ($b*$d);
+$g = $e+$f;
+
+echo "Uang hasil penjualan jambu : " . $e . "<br>";
+echo "Uang hasil penjualan jambu : " . $f . "<br>";
+echo "Uang hasil penjualan jambu dan dus: " . $g;
+?>
---
+++ tugas-2.php
@@ -0,0 +1,9 @@
+<?php
+$tama = 32;
+$toni = 32/2;
+$tini = 32-14;
+
+echo "tama = $tama <br>";
+echo "toni = $toni <br>";
+echo "tini = $tini";
+?>
